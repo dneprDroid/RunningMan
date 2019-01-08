bf29e480e9003383950fc200ec27564da8bc4d5a
Alex iOS <alex.ovechko@softensy.com>
2019-01-08 15:04:05 +0200

    Update to Swift 4.2.

--- RunningMan/AppDelegate.swift
+++ RunningMan/AppDelegate.swift
@@ -13,8 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var window: NSWindow!
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
     }
-    
 }
